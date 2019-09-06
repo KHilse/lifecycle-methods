741c3864da17e70afef5bf8ac4ac6d1cfc2f241a
KHilse <khilsedev@gmail.com>
2019-09-06 11:24:25 -0700

    Built test app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Parent extends React.Component {
+  constructor(props) {
+   super(props);
+
+    this.state = {
+      index: 0,
+      message: ""
+    }
+  }
+
+  handleChildClick = () => {
+
+    this.setState({ index: this.state.index + 1 })
+
+  }
+
+  shouldComponentUpdate() {
+    let shouldUpdate = false;
+    if (this.state.index % 5 === 0) {
+      shouldUpdate = true;
+      this.setState({ message: "FIZZ!"})
+      console.log("TRUE!");
+    } else {
+      shouldUpdate = false;
+      console.log("false");
+    }
+
+
+    console.log("ShouldComponentUpdate called");
+    return (shouldUpdate)
+  } 
+
+  getSnapshotBeforeUpdate(previousProps, previousState) {
+    console.log("getSnapshotBeforeUpdate");
+    return null
+  }
+
+
+  componentDidUpdate(previousProps, previousState, snapshot) {
+    console.log("componentDidUpdate");
+  }
+
+  render() {
+    return (
+    <div>Parent Class
+      <Child message={this.state.message} onClick={this.handleChildClick}/>
+    </div>
+
+    )
+  }
+}
+
+class Child extends React.Component {
+
+  render() {
+
+    return (
+      <div onClick={this.props.onClick}>Child Class {this.props.message}</div>
+    )
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parent />
     </div>
   );
 }
